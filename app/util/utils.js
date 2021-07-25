81a6e2231ef8bcadc116b861b6850b6be69a2bed
Watcharin Worapakdee <watcharinwor@gosoft.co.th>
2021-07-25 20:18:49 +0700

    Update utils.js

--- app/util/utils.js
+++ app/util/utils.js
@@ -1,10 +1,9 @@
 var moment = require('moment');
-const formatTimeStamp = "MM-DD-YYYY HH:MM:ss"
 
 var util = {
     getCurrentDate: function(){
         var today = new Date();
-        var date = moment(today, formatTimeStamp);
+        var date = moment(today).format("DD/MM/YYYY HH:mm:ss");
         return date;
     }
 }
