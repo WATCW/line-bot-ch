38c19373e737a69f200e86d4f4911f9c8cb4e66c
Watcharin Worapakdee <watcharinwor@gosoft.co.th>
2021-07-25 20:37:44 +0700

    Update utils.js

--- app/util/utils.js
+++ app/util/utils.js
@@ -3,7 +3,7 @@ var moment = require('moment');
 var util = {
     getCurrentDate: function(){
         var today = new Date();
-        var date = `${(today.getMonth() + 1).toString().padStart(2, '0')}/${today.getDate().toString().padStart(2, '0')}/${today.getFullYear().toString().padStart(4, '0')} ${today.getHours().toString().padStart(2, '0')}:${today.getMinutes().toString().padStart(2, '0')}:${today.getSeconds().toString().padStart(2, '0')}`;
+        var date = `${(today.getMonth() + 1).toString().padStart(2, '0')}/${today.getDate().toString().padStart(2, '0')}/${today.getFullYear().toString().padStart(4, '0')}`;
         return date;
     }
 }
