0ff866a47245dfa4a8d6069ef33d91d9d957959b
Watcharin Worapakdee <31218118+WATCW@users.noreply.github.com>
2024-11-08 16:18:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use((req, res, next) => {
   
     next();
   });
-  
+
+app.use(express.static('uploads'))
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 app.get('/', (req, res) => {
